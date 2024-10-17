b7cb5b1b6a26ac105433b9c5cf75948146c7bd2d
Serge K <datascience2x@gmail.com>
2024-10-17 18:14:38 +0500

    first exercise done

---
+++ reverse_words.py
@@ -0,0 +1,24 @@
+'''
+Задача 1: Реверсирование строки
+Напишите функцию, которая принимает строку и возвращает её, но со словами в обратном порядке.
+'''
+
+def reverse_words(my_str: str) -> str:
+    '''
+    Функция разворачивает порядок слов в строке, поданой на вход.
+
+    Args:
+        Строка my_str (str).
+
+    Returns:
+        new_str (str): развернутая строка.
+    '''
+    new = ' '.join(my_str.split()[::-1])
+
+    return new
+
+
+# Тест
+s = "Hello world from Python"
+print(reverse_words(s))  # "Python from world Hello"
+print(type(reverse_words(s))) # <class 'str'>
